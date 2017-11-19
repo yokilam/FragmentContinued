7bc8c1822df4be3c49f5c8a5830e7690d13c1752
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2017-11-19 15:08:26 -0500

    first commit

---
+++ app/src/main/java/nyc/c4q/fragmentcontinued/MainActivity.java
@@ -0,0 +1,20 @@
+package nyc.c4q.fragmentcontinued;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BaseFragment baseFragment= new BaseFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, baseFragment);
+    }
+}
